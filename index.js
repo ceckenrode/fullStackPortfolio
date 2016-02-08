f043eae643072377c46ee2872d6533cf8d498f2c
ceckenrode <chriseckenrode@me.com>
2016-02-08 16:06:18 -0500

    added index.js file for my node server, put static elements into the public folder

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use("/js", express.static("public/js"));
+app.use("/css", express.static("public/css"));
+app.use("/images", express.static("public/images"));
+
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+
+app.listen(PORT, function() {
+  console.log("App listening on port %s", PORT);
+});
